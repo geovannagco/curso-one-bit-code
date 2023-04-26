0c512dac09978403e3b2e49e278f527dd5e4820e
Geovanna Correa de Oliveira <geovanna.gco@hotmail.com>
2023-04-26 09:38:58 -0300

    Recursos Nativos
    window, alert e document

---
+++ js-i-logica-de-programacao/recursos-nativos-nav/index.js
@@ -0,0 +1,11 @@
+let nome = 'Geovanna';
+
+console.log(nome);
+console.error('isso é um erro!!!');
+console.warn('isso é um alerta!!!');
+
+window.alert('isso é uma mensagem!!!');
+nome = window.prompt('Insira o seu nome:');
+let confirmacao = window.confirm('Você se chama Geovanna?');
+
+console.log(nome);
