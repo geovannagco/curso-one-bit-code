f6a37cb35e02b20ffafccd9cd48559b2ffca7b16
Geovanna Correa de Oliveira <geovanna.gco@hotmail.com>
2023-05-04 16:48:20 -0300

    If e Else
    Lógica de Programação - JS

---
+++ js-i-logica-de-programacao/aula018/index.js
@@ -0,0 +1,24 @@
+// const idade = prompt('Informe a sua idade: ');
+
+// if (idade > 18) {
+//   alert('Você é maior de idade!');
+// } else if (idade > 12) {
+//   alert('Você é menor de idade!');
+// } else if (idade >= 4) {
+//   alert('Você é criança!');
+// } else if (idade < 4) {
+//   alert('Você é um bebê!');
+// }
+
+// const resultado = 6 === 6 ? 'Verdadeiro' : 'falso'; //operador ternário é a abreviação da estrutura if else
+// console.log(resultado);
+
+const hora = prompt('Informe a hora: ');
+
+if (hora <= 12) {
+  alert('Bom dia!');
+} else if (hora <= 18) {
+  alert('Boa tarde!');
+} else {
+  alert('Boa noite!');
+}
