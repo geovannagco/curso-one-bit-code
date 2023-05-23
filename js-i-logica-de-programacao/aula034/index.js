ece275967bc95221ec743334c87c53a1147b1772
geovannagco <geovanna.gco@hotmail.com>
2023-05-23 11:30:04 -0300

    aula034

---
+++ js-i-logica-de-programacao/aula034/index.js
@@ -0,0 +1,13 @@
+const frutas = ['maçã', 'banana', 'laranja', 'uva']; //Array chamado 'frutas' com 4 elementos.
+// A posição [0] contém a string 'maçã'
+// A posição [1] contém a string 'banana'
+// A posição [2] contém a string 'laranja'
+// A posição [3] contém a string 'uva'
+
+console.log(frutas[0]); // Saída: 'maçã'
+console.log(frutas[2]); // Saída: 'laranja'
+
+frutas[1] = 'morango'; // Altera o valor do elemento [1] no array
+
+console.log(frutas[1]);
+console.log(frutas);
