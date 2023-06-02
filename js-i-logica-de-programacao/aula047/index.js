cddc17db52682f1469979aa8b3fb6af849b0dbd3
geovannagco <geovanna.gco@hotmail.com>
2023-06-02 14:52:07 -0300

    aula047
    Escopo

---
+++ js-i-logica-de-programacao/aula047/index.js
@@ -0,0 +1,51 @@
+let pokemon = 'Charmander'; // Variável de escopo global
+
+function evoluir() {
+  pokemon = 'Charmeleon';
+}
+
+console.log(pokemon);
+evoluir();
+console.log(pokemon);
+
+console.clear();
+
+// function criarAnimal() {
+//   let animal = 'Gato';
+// }
+
+// criarAnimal();
+// console.log(animal);
+// console.clear;
+
+// function avaliarNota(nota) {
+//   if (nota > 60) {
+//     var aprovado = true;
+//     let situacao = 'Aprovado';
+//   } else {
+//     var aprovado = false;
+//     let situacao = 'Reprovado';
+//   }
+
+//   console.log(nota);
+//   console.log(aprovado);
+//   console.log(situacao);
+// }
+
+// avaliarNota(83);
+// avaliarNota(49);
+
+// function ola() {
+//   let texto = 'Ola, mundo';
+// }
+
+// console.log(texto);
+
+console.log(nome);
+// console.log(sobrenome);
+
+var nome = 'Geovanna';
+let sobrenome = 'Correa';
+
+console.log(nome);
+// console.log(sobrenome);
