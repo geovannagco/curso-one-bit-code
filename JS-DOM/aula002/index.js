3255166d18e7f0c64af027a85a7908c61238f8a4
geovannagco <geovanna.gco@hotmail.com>
2023-07-04 16:43:53 -0300

    aula002

---
+++ JS-DOM/aula002/index.js
@@ -0,0 +1,19 @@
+function onMouseEnter() {
+  console.log('Entrada do Mouse');
+}
+
+function onChange() {
+  console.log('Mudança no Valor');
+}
+
+function onFocus() {
+  console.log('Foco no elemento');
+}
+
+function onBlur() {
+  console.log('Desfocado!');
+}
+
+function onClick() {
+  console.log('O botão foi clicado!');
+}
